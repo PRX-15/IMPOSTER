73682a566736dca571c9ddc6ac29d57195f481fc
PRX <prashx15@gmail.com>
2026-09-01 00:35:10 +0530

    Use game font for reveal text

--- screens/reveal.py
+++ screens/reveal.py
@@ -70,6 +70,7 @@ class PlainCurtain(Button):
         kwargs.setdefault("bold", True)
         kwargs.setdefault("halign", "center")
         kwargs.setdefault("valign", "middle")
+        kwargs.setdefault("font_name", "GameFont")
         super().__init__(**kwargs)
         with self.canvas.before:
             Color(*COLORS["card"])
